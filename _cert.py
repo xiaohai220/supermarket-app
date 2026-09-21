ea0d60f500e38d632085bbd81b7af26a557c99d1
Supermarket App <dev@localhost>
2026-09-22 01:59:41 +0500

    清理临时脚本

--- _cert.py
+++
@@ -1,7 +0,0 @@
-import paramiko
-c=paramiko.SSHClient(); c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect("43.128.10.238",22,"ubuntu","Xh137465",timeout=25)
-cmd = "sudo certbot certonly --webroot -w /home/ubuntu/supermarket-app -d xxhrcs.com -d www.xxhrcs.com --non-interactive --agree-tos -m admin@xxhrcs.com 2>&1 | tail -25"
-i,o,e=c.exec_command(cmd,timeout=120)
-print(o.read().decode(), e.read().decode())
-c.close()
